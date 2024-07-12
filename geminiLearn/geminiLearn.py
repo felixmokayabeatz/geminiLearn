2725c3ed3a12a72ac41250aaa991c1423458226e
Felix Mokaya <felixmokayabeatz@gmail.com>
2024-07-12 11:48:18 +0300

    Confugured the Gemini project key fropm other google account to learn more on Gemini

--- geminiLearn/geminiLearn.py
+++ geminiLearn/geminiLearn.py
@@ -2,8 +2,15 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-API_KEY = os.getenv('API_KEY')
+
+
+
+dotenv_path = 'G:/geminiLearn/geminiLearn/API_KEY_GEMINI/.env'
+
+# Load environment variables from the specified .env file
+load_dotenv(dotenv_path)
+
+API_KEY = os.getenv('API_KEY_GEMINI')
 
 if API_KEY is None:
     print("No API_KEY Found!")
